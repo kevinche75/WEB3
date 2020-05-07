a2a5e738420c3111d9fbf56e72aaecc9839f6dea
kevinche75 <kevinche75@gmail.com>
2020-05-08 02:06:27 +0400

    pizdec

--- src/test/PointTest.java
+++ src/test/PointTest.java
@@ -9,10 +9,10 @@ import org.junit.Test;
 public class PointTest {
     @Test
   public void getA(){
-        Point point = new Point(1.3,1.6,3.8);
+        Point point = new Point(1.3,-1.6,3.8);
 //        Point point1 = new Point(1.3,1.6,3.8);
 //        Point point2 = new Point(1d,7d,4d);
-        Assert.assertEquals(point,point);
-        Assert.assertNull(null);
+        Assert.assertEquals(point.getInArea(),true);
+//        Assert.assertNull(point);
     }
 }
