c253355f94edb3af7c6a17891c12c5fcef09d367
Елизавета <medvedeva_elizaveta2000@mail.ru>
2020-05-07 23:48:51 +0300

    test

--- src/test/PointTest.java
+++ src/test/PointTest.java
@@ -1,14 +1,18 @@
 package test;
 
+
 import main.Point;
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class PointTest {
     @Test
-  public   void getA(){
-        Point point1 = new Point(1,2,3);
-        Point point2 = new Point(2,7,4);
-        Assert.assertEquals(point1,point2);
+  public void getA(){
+        Point point = new Point(1.3,1.6,3.8);
+//        Point point1 = new Point(1.3,1.6,3.8);
+//        Point point2 = new Point(1d,7d,4d);
+        Assert.assertEquals(point,point);
+        Assert.assertNull(null);
     }
 }
