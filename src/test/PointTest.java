0266a74a3c093b7bc425cd8d8376682c0a481271
Елизавета <medvedeva_elizaveta2000@mail.ru>
2020-05-08 17:39:05 +0300

    test

--- src/test/PointTest.java
+++ src/test/PointTest.java
@@ -14,24 +14,25 @@ public class PointTest {
     }
 
     @Test
-    public  void checkRandomPoint(){
-        Point point = new Point(Math.random()*8-5,Math.random()*10-5,Math.random()*5);
-        boolean isInArea = false;
-
-        if(point.getX()>=0 && point.getY()>=0)
-            isInArea = point.getX()<=point.getR() && point.getY()<=point.getR()/2;
-
-        if(point.getX()<0 && point.getY()>=0)
-            isInArea = point.getY() <= point.getX()/2 + point.getR()/2;
-
-        if(point.getX()<=0 && point.getY()<=0)
-            isInArea = Math.pow(point.getX(), 2) + Math.pow(point.getY(),2) <= Math.pow(point.getR(), 2);
-
-        Assert.assertEquals(point.getInArea(),isInArea);
+    public  void checkPoints(){
+        Point point = new Point(2d,4d,3d);
+        Point point1 = new Point(1.5,3.2,3.8);
+         Point point2 = new Point(0.45,2.56,3.9);
+        Point point3 = new Point(2d,4.6,1.3);
+        Point point4 = new Point(2.6,0.3,3.4);
+        Assert.assertEquals(point.getInArea(),isInArea(point));
+        Assert.assertEquals(point1.getInArea(),isInArea(point1));
+        Assert.assertEquals(point2.getInArea(),isInArea(point2));
+        Assert.assertEquals(point3.getInArea(),isInArea(point3));
+        Assert.assertEquals(point4.getInArea(),isInArea(point4));
     }
 
-
-
+   private   boolean isInArea(Point point){
+        if (point.getX()>0&&point.getY()<0) return false;
+        if(point.getX()>=0 && point.getY()>=0) return point.getX()<=point.getR() && point.getY()<=point.getR()/2;
+        if(point.getX()<=0 && point.getY()<=0) return Math.pow(point.getX(), 2) + Math.pow(point.getY(),2) <= Math.pow(point.getR(), 2);
+        return false;
+    }
 
     
 }
