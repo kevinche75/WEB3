acfdbbbf37ddd0fb6974d3d688064bdf921ba139
kevinche75 <kevinche75@gmail.com>
2020-05-08 11:45:04 +0400

    libs

--- src/main/HibernateUtil.java
+++ src/main/HibernateUtil.java
@@ -26,5 +26,3 @@ public class HibernateUtil {
         return sessionFactory;
     }
 }
-
-//some
--- src/test/PointTest.java
+++ src/test/PointTest.java
@@ -12,7 +12,7 @@ public class PointTest {
         Point point = new Point(1.3,-1.6,3.8);
 //        Point point1 = new Point(1.3,1.6,3.8);
 //        Point point2 = new Point(1d,7d,4d);
-        Assert.assertEquals(point.getInArea(),false);
+        Assert.assertEquals(point.getInArea(),true);
 //        Assert.assertNull(point);
     }
 }
