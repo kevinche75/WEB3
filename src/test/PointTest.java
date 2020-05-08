52786c2282de009f873f1eca791b6a68ef1440fa
Елизавета <medvedeva_elizaveta2000@mail.ru>
2020-05-08 10:54:17 +0300

    test

--- src/test/PointTest.java
+++ src/test/PointTest.java
@@ -8,11 +8,30 @@ import org.junit.Test;
 
 public class PointTest {
     @Test
-  public void getA(){
+  public void checkPoint(){
         Point point = new Point(1.3,-1.6,3.8);
-//        Point point1 = new Point(1.3,1.6,3.8);
-//        Point point2 = new Point(1d,7d,4d);
-        Assert.assertEquals(point.getInArea(),true);
-//        Assert.assertNull(point);
+        Assert.assertEquals(point.getInArea(),false);
     }
+
+    @Test
+    public  void checkRandomPoint(){
+        Point point = new Point(Math.random()*8-5,Math.random()*10-5,Math.random()*5);
+        boolean isInArea = false;
+
+        if(point.getX()>=0 && point.getY()>=0)
+            isInArea = point.getX()<=point.getR() && point.getY()<=point.getR()/2;
+
+        if(point.getX()<0 && point.getY()>=0)
+            isInArea = point.getY() <= point.getX()/2 + point.getR()/2;
+
+        if(point.getX()<=0 && point.getY()<=0)
+            isInArea = Math.pow(point.getX(), 2) + Math.pow(point.getY(),2) <= Math.pow(point.getR(), 2);
+
+        Assert.assertEquals(point.getInArea(),isInArea);
+    }
+
+
+
+
+    
 }
